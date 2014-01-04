cf01fa69b57f1d34fd675a8d60eaa70c289dfa07
JPB <jpboneyard@gmail.com>
2014-01-04 01:50:13 -0500

    setting up 404 page.

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php 
+$title = "The National Poster Retrospecticus";
+include($_SERVER["DOCUMENT_ROOT"]."/_includes/page-start.php"); ?>
+
+<?php include($_SERVER["DOCUMENT_ROOT"]."/_includes/header.php"); ?>
+
+<!doctype html>
+<title>Page Not Found</title>
+<div class="error-page-mod">
+    <div class="error-video-mod">
+        <video class="error-video" width="100%" height="100%" autoplay loop>
+            <source src="/_img/video/404-larry-bird.mp4" type="video/mp4">
+          <source src="/_img/video/404-larry-bird.mp4" type="video/ogg">
+          Your browser does not support the video tag.
+        </video> 
+    </div> <!-- /error-vide-mod -->
+    <div class="error-msg-mod">
+    	<h3 class="error-msg">404: Page Not Found</h3>
+    	<p class="error-tag">Someone crashed the internet. Please don't call the cops.</p>
+    </div> <!-- /error-msg-mod -->	
+</div> <!-- /error-page-mod -->
