bc3068e27ec5a95e06a6867c5366f7f6a5b68f15
JP <Boneyard@JPs-iMac.attlocal.net>
2016-07-04 15:23:47 -0500

    Adding Fifty-Nine Parks to site

---
+++ fiftynineparksprintseries/index.php
@@ -0,0 +1,83 @@
+<?php 
+$title = "The National Poster Retrospecticus";
+include($_SERVER["DOCUMENT_ROOT"]."/_includes/page-start.php"); ?>
+
+<?php include($_SERVER["DOCUMENT_ROOT"]."/_includes/header.php"); ?>
+
+		<div id="main" role="main">
+			<div class="feat-project-mod" id="top">
+				<img class="feat-project-img" src="/_img/national-poster-retrospecticus-topper.gif" alt="Fifty-Nine Parks Print Series Topper" />
+			</div> <!-- /feat-project-mod -->
+
+			<div class="project-page-mod">
+				<div class="project-page-content">
+
+					<div class="overline">Art Direction & Production</div>
+					<h1>The Fifty-Nine Parks Print Series</h1>
+					<p>This brand combines some of my favorite things: National Parks and screen printed posters! <a href="http://www.59parks.com" target="_blank">The Fifty-Nine Parks Print Series</a> is </p>
+
+					<p>Touring around the country with The NPR has been one of the greatest adventures of my life. It is through this adventure that I am able to combine my design skills, show planning background, love for community based events, enthusiasm for travel and passion for posters into one amazing experience. I am also fortunate enough to meet some of the most genuine and kindest people while on the road—most of whom share the same enthusiasm for posters. This enthusiasm is like fuel that helps keep me going while we make those eleven hour drives through crazy weather and deserted stretches of highway, hang 300 posters and host a packed show for one-night-only in each city.</p>
+					
+
+					<img class="img-full full-m section-start" src="/_img/national-poster-retrospecticus-poster-jp-boneyard.jpg" alt="National Poster Retrospecticus Poster" />
+
+					<h3>Art Direction</h3>
+					<p>Retrospecticus is a funny word. The translation comes out to something along the lines of "to look forward" and "to look backward". Combine that with imagery from early American lore and we have the all seeing eye of providence. The brand is flexible enough to fit the needs of a 1" button, an extensive website, a poster series or a giant banner.</p>
+
+
+					<a href="http://www.nationalposterretrospecticus.com" target="_blank">
+						<video class="site-video img-med section-start npr-site">
+						  	<source src="/_img/video/npr-site-scroll.mp4" type="video/mp4">
+							<source src="/_img/video/npr-site-scroll.ogv" type="video/ogg">
+							Your browser does not support the video tag.
+						</video> 
+					</a>	
+
+					<a href="http://www.nationalposterretrospecticus.com" target="_blank">
+						<img class="site-video-poster img-med section-start" src="/_img/national-poster-retrospecticus-web.jpg" />
+					</a> <!-- /site-video-poster-link -->	
+
+					<h3>Production</h3>
+					<p>The design for the website has evolved right along with the needs of the site. My first pass was just something to announce and document the second coming of the show in 2012. The second, third and fourth passes were ways to document the tours, do a little lifting in the marketing department and do a better job of explaining what the heck this crazy show is all about.</p>
+
+					<btn class="btn-submit btn-txt"> <a href="http://www.nationalposterretrospecticus.com" target="_blank">Launch The Site</a></btn>
+
+
+
+					<div class="hdr-sign-container section-start">
+						<img class="hdr-pin" src="/_img/hdr-pin-2.png" alt="pin" />
+						<div class="hdr-sign">
+							<img class="hdr-slide-img" src="/_img/hdr-sign-2.png" alt="Tour Sign" />
+						</div> <!-- /hdr-sign -->	
+					</div> <!-- /hdr-sign-container -->	
+
+					<h3>Design</h3>
+					<p>This is where the heavy lifting comes in. Booking a cross-country art tour isn't standard fare—especially for one-night-only events. I've found that my previous experience booking bands came in handy. The logistical puzzle of booking consecutive shows, promoting each event across the interweb and in all of the local news and art publications can get heavy but I love digging into complex problems like this!</p>
+
+
+					<img class="img-full section-start" src="/_img/national-poster-retrospecticus-banner-jp-boneyard.jpg" alt="National Poster Retrospecticus Branding" />
+
+
+					<img class="img-full img-ftr-top section-start" src="/_img/national-poster-retrospecticus-crowd.jpg" alt="National Poster Retrospecticus Photo of Boston Show" />
+					
+				</div> <!-- /project-page-content -->					
+
+			</div> <!-- /project-page-mod -->
+
+			<div class="more-work-mod">
+				<img src="/_img/more-work.png" />
+			</div> <!-- /more-work-mod -->
+
+			<div class="inside-projects">
+				<?php include($_SERVER["DOCUMENT_ROOT"]."/_includes/project-tiles.php"); ?>
+			</div>	<!-- /inside-projects -->
+
+		<!-- footer -->
+		<?php include($_SERVER["DOCUMENT_ROOT"]."/_includes/footer.php"); ?>
+
+
+	<!-- Page End -->
+	<?php include($_SERVER["DOCUMENT_ROOT"]."/_includes/page-end.php"); ?>
+
+		
+ 
