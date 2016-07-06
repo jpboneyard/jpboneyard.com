16f073e33c0efc820983befa495e89656d3c1ba7
JP <Boneyard@JPs-iMac.attlocal.net>
2016-07-06 10:15:45 -0500

    updating HDR links and parks page content

--- fiftynineparksprintseries/index.php
+++ fiftynineparksprintseries/index.php
@@ -14,34 +14,33 @@ include($_SERVER["DOCUMENT_ROOT"]."/_includes/page-start.php"); ?>
 
 					<div class="overline">Creative Direction & Production</div>
 					<h1>Fifty-Nine Parks Print Series</h1>
-					<p>More of my favorite things: The US National Parks and screen printed posters! <a href="http://www.59parks.net" target="_blank">The Fifty-Nine Parks Print Series</a> is falls under the umbrella of The National Poster Retrospecticus. The strength of the series comes from an eclectic mix of artists and the unique beauty of each park. The idea for this one is simple: celebrate the parks and some of our favorite poster artists in the world! Some of our early aspirations for the series included touring the parks with the show (in the works!) and getting the collection into the Library of Congress down the line. It's crazy to think the second aspiration has already come true! It's a total blast to work with so many talented artists and build another brand from scratch.</p>
+					<p>The US National Parks and screen printed posters are two of my favorite things. <a href="http://www.59parks.net" target="_blank">The Fifty-Nine Parks Print Series</a> falls under the umbrella of The National Poster Retrospecticus. The strength of the series comes from an eclectic mix of artists and the unique beauty of each park. The idea for this one is simple: celebrate the parks and some of our favorite poster artists in the world! Some of our early aspirations for the series included touring the parks with the show (in the works!) and getting the collection into the Library of Congress at some point. It's crazy to think the second aspiration has already come true! It's a total blast to work with so many talented artists and build another brand from scratch.</p>
 
 					<img class="img-full full-m section-start" src="/_img/fifty-nine-parks-print-series-wall.jpg" alt="Fity-Nine Parks Print Series Wall" />
 
 					<h3>Creative Direction</h3>
-					<p>Part of what makes this series exciting is working with so many different artists. I work with each artist through the research phase, concept development, initial sketches, and final art. The goal of each poster is to capture a scene that best represents the park and best suits each artists aesthetic strengths. It's no small task to create a static image that captures the essence of each park. Every artist has a different working style too. It's a fun challenge to adapt to those different styles while bringing in elements of both collaboration and cohesivness to the series.</p>
+					<p>Part of what makes this series exciting is working with so many different artists. I work with each artist through the research phase, concept development, initial sketches, and final art. The goal of each poster is to capture a scene that best represents the park and best suits each artists aesthetic strengths. It's no small task to create a static image that captures the essence of each park. Every artist has a different working style too. It's a fun challenge to adapt to those different styles while bringing in elements of collaboration and cohesivness to the series.</p>
 
-					<img class="img-full full-m section-start" src="/_img/fifty-nine-parks-print-series-detail-justin-santora-biscayne-national-park.jpg" alt="Fity-Nine Parks Print Series Print Detail" />
+					<img class="img-full section-start" src="/_img/fifty-nine-parks-print-series-merch-5.jpg" alt="Fifty-Nine Parks Print Series Merch" />
+
 
 					<h3>Building the Brand</h3>
 					<p>One of the challenges of building a brand and maximizing your own output is knowing when to bring others into the fold. That has meant bringing in designers like <a href="http://www.neighborhood-studio.com/" target="_blank">Curtis Jinkins</a> to design the parks branding. We also teamed up with type designer <a href="http://www.rileycran.com/" target="_blank">Riley Cran</a> to design a custom typeface for the series. This meant I was freed up to build the parks website, create and typeset a poster template for the series, and expand upon the beautiful branding that Curtis created for us.</p>
-
+					
+					<img class="img-sm section-end" src="/_img/fifty-nine-parks-print-series-catlin-sans-riley-cran.jpg" alt="Fity-Nine Parks Print Series Print Detail" />
 					
 					<a href="http://www.59parks.net" target="_blank">
 						<img class="img-sm section-start" src="/_img/fifty-nine-parks-print-series-site.jpg" />
 					</a> <!-- /site-video-poster-link -->	
-			
-
-					<h3>Producing the Series</h3>
-					<p>This means everything from funding the project to promoting it. One day you'll find me working with our printers to fine-tune colors. The next I'll be queuing up new artists, working with wholesalers, and creating promotions for the series. One of the most exciting challenges is thinking strategically about what moves to make next. Building a brand takes time. I can picture the finish line with all of the posters created and the show touring around the US. All of those posters can't be made over night though. We're limted by things like working capital and production times. But those limitations are fun to work with—they often lead to some fun creative problem solving. It's exciting to dream up and create new products that we can introduce to diversify the appeal of the brand.</p>
 
 					<btn class="btn-submit btn-txt"> <a href="http://59parks.net" target="_blank">Launch The Site</a></btn>
 
 
-					<!-- I'll really feel like the series has accomplisehd something if we can get parks fans into posters and poster fans in to the parks. That's one of the biggest factors that influences the direction of the series and what moves we make next. -->
+					<h3>Producing the Series</h3>
+					<p>This means everything from funding the project to promoting it. One day you'll find me working with our printers to fine-tune colors. The next I'll be queuing up new artists, working with wholesalers, and creating promotions for the series. One of the most exciting challenges is thinking strategically about what moves to make next. Building a brand takes time. I can picture the finish line with all of the posters created and the show touring around the US. All of those posters can't be made over night though. We're limted by things like working capital and production times. But those limitations are fun to work with—they often lead to some fun creative problem solving. It's exciting to dream up and create new products that we can introduce to diversify the appeal of the brand.</p>
 
 
-					<img class="img-full img-ftr-top section-start" src="/_img/fifty-nine-parks-print-series-merch-5.jpg" alt="Fifty-Nine Parks Print Series Merch" />
+					<img class="img-full img-ftr-top" src="/_img/fifty-nine-parks-print-series-detail-justin-santora-biscayne-national-park.jpg" alt="Fity-Nine Parks Print Series Print Detail" />
 					
 				</div> <!-- /project-page-content -->					
 
